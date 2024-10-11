9a013c2506188262d1fbb79dc1169507b975a274
gunnha <zxcvbnm9931@epozen.com>
2024-10-11 15:23:39 +0900

    css, js 수정

---
+++ api/src/main/java/com/example/api/config/MessageConfig.java
@@ -0,0 +1,23 @@
+package com.example.api.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * @author gunha
+ * @version 1.0
+ * @since 2024-10-11 오후 3:04
+ */
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages_en");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
--- api/src/main/resources/static/js/script.js
+++ api/src/main/resources/static/js/script.js
@@ -1,4 +1,11 @@
+/**
+ * 지정된 Drop 영역에 파일을 드래그 앤 드롭하여 업로드 <br>
+ * 드래그 앤 드롭 이벤트 처리 및 업로드된 이미지 파일 미리보기
+ *
+ * @param {string} dropAreaId - 드롭 영역의 HTML 요소 ID
+ */
 function DropFile(dropAreaId) {
+
     let dropArea = document.getElementById(dropAreaId);
 
     function preventDefaults(e) {
@@ -6,16 +13,19 @@ function DropFile(dropAreaId) {
         e.stopPropagation();
     }
 
+    // Drop 영역에 파일이 들어올 때, 강조 표시
     function highlight(e) {
         preventDefaults(e);
         dropArea.classList.add("highlight");
     }
 
+    // Drop 영역에서 파일이 나갈 때, 강조 표시
     function unhighlight(e) {
         preventDefaults(e);
         dropArea.classList.remove("highlight");
     }
 
+    // 파일을 Drop 했을 때, 호출
     function handleDrop(e) {
         unhighlight(e);
         let dt = e.dataTransfer;
@@ -24,29 +34,34 @@ function DropFile(dropAreaId) {
         handleFiles(files);
     }
 
+    // Drop된 파일을 처리
     function handleFiles(files) {
         files = [...files];
         files.forEach(previewFile);
     }
 
+    // 파일 미리보기용
     function previewFile(file) {
-        if (file.type.startsWith("image/")) { // 이미지 타입인지 확인
+        // 이미지 타입인지 확인
+        if (file.type.startsWith("image/")) {
             renderFile(file);
         } else {
             alert("이미지 파일만 업로드할 수 있습니다.");
         }
     }
 
+    // 파일 렌더링
     function renderFile(file) {
         let reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onloadend = function () {
             let img = dropArea.getElementsByClassName("preview")[0];
             img.src = reader.result;
-            img.style.display = "block";
+            img.style.display = "block";  // 미리보기 이미지를 보이도록 설정
         };
     }
 
+    // Drag 관련 EventListener
     dropArea.addEventListener("dragenter", highlight, false);
     dropArea.addEventListener("dragover", highlight, false);
     dropArea.addEventListener("dragleave", unhighlight, false);
